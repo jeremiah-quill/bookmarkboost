dbe509a0674d910671197e37fce2099fe0cc4bd0
Jeremiah Quill <jcq5010@gmail.com>
2022-06-06 15:57:10 -0400

    added tailwind, initial navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,16 +2,19 @@ import { useAuth } from "../lib/useAuth.js";
 
 const Navbar = () => {
   const auth = useAuth();
-  console.log(auth);
   return (
-    <nav>
-      <h1>Bookmark Boost</h1>
+    <nav className="flex justify-between fixed top-0 w-full p-5 items-center border-b-slate-400 border-b">
+      <h1 className="text-2xl">BB</h1>
       {!auth.user ? (
         <button onClick={() => auth.signInWithGoogle()}>Login</button>
       ) : (
-        <div>
-          <h2>{auth.user.email}</h2>
-          <button onClick={() => auth.signOut()}>Logout</button>
+        <div className="flex gap-2 items-center">
+          <h2 className="text-sm">{auth.user.email}</h2>
+          <button
+            className="bg-slate-800 rounded-md py-1 px-2 text-white"
+            onClick={() => auth.signOut()}>
+            Logout
+          </button>
         </div>
       )}
     </nav>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import styles from "../styles/Home.module.css";
-import { supabase } from "../lib/supabase";
-import { useEffect, useState } from "react";
 
 export default function Home() {
   return (
